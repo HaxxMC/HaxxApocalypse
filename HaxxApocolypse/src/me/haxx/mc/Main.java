89870cfc95331b5f986175239f61b19bc2172e28
EssexIO <essexwebsec@gmail.com>
2014-05-18 15:09:01 -0700

    Fixed Plugin.yaml, restored event registration.
    Change-Id: Iab0dea8d729fdecbcea745167f4b87a86791aa1c
    Signed-off-by: EssexIO <essexwebsec@gmail.com>

--- HaxxApocolypse/src/me/haxx/mc/Main.java
+++ HaxxApocolypse/src/me/haxx/mc/Main.java
@@ -1,9 +1,19 @@
 package me.haxx.mc;
 
+import org.bukkit.Bukkit;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
+	
+	public static void registerEvents(org.bukkit.plugin.Plugin plugin,
+			Listener... listeners) {
+		for (Listener listener : listeners) {
+			Bukkit.getServer().getPluginManager()
+					.registerEvents(listener, plugin);
+		}
+	}
 	
 	private static Plugin plugin;
 
@@ -13,7 +23,7 @@ public class Main extends JavaPlugin {
 
 	public void onEnable(){
 		getServer().getWorld("world").setTicksPerMonsterSpawns(200);
-		getServer().getPluginManager().registerEvents(new Chat(), this);
+		registerEvents(this, new Chat());
 	}
 	
 	public void onDisable(){}
