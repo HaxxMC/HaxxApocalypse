93deeb6962aae1c8913a15564048f533aa64bedf
Chris <lavacraft219@gmail.com>
2014-05-18 18:09:19 -0400

    Fixed plugin.yml Modified listener registration
    Change-Id: I5cfc98506a1f6d267a5647a6627b52087c4ed46e
    Signed-off-by: Chris <lavacraft219@gmail.com>

--- HaxxApocolypse/src/me/haxx/mc/Main.java
+++ HaxxApocolypse/src/me/haxx/mc/Main.java
@@ -1,5 +1,6 @@
 package me.haxx.mc;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -12,8 +13,8 @@ public class Main extends JavaPlugin {
 	}
 
 	public void onEnable(){
-		getServer().getWorld("world").setTicksPerMonsterSpawns(200);
-		getServer().getPluginManager().registerEvents(new Chat(), this);
+		Bukkit.getServer().getWorld("world").setTicksPerMonsterSpawns(200);
+		Bukkit.getServer().getPluginManager().registerEvents(new Chat(), this);
 	}
 	
 	public void onDisable(){}
