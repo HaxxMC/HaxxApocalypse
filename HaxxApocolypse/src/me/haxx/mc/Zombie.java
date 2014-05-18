3881c3182aaca7d81461fc1ce1a5889360137fac
Chris <lavacraft219@gmail.com>
2014-05-18 17:34:20 -0400

    Changed how event onHit is carried out.
    Change-Id: I47a1d4b7aa1d49b4536c2949a3d49d3e06419b25
    Signed-off-by: Chris <lavacraft219@gmail.com>

--- HaxxApocolypse/src/me/haxx/mc/Zombie.java
+++ HaxxApocolypse/src/me/haxx/mc/Zombie.java
@@ -14,30 +14,30 @@ public class Zombie implements Listener{
 	@EventHandler
 	public void onHit(EntityDamageByEntityEvent e){
 		Entity a = e.getDamager();
-		Entity t = e.getEntity();
 		Location l = a.getLocation();
 		
+		double dmg = e.getDamage();
 		int x = l.getBlockX();
 		int z = l.getBlockZ();
 		
-		if(a instanceof org.bukkit.entity.Zombie && t instanceof Player){
+		if(a instanceof org.bukkit.entity.Zombie){
 			if(x>999 && x<2000 || z>999 && z<2000){
-				e.setDamage(e.getDamage() + 1);
+				e.setDamage(dmg + 1);
 			}
 			if(x>1999 && x<3000 || z>1999 && z<3000){
-				e.setDamage(e.getDamage() + 2);
+				e.setDamage(dmg + 2);
 			}
 			if(x>2999 && x<4000 || z>2999 && z<4000){
-				e.setDamage(e.getDamage() + 3);
+				e.setDamage(dmg + 3);
 			}
 			if(x>3999 && x<5000 || z>3999 && z<5000){
-				e.setDamage(e.getDamage() + 4);
+				e.setDamage(dmg + 4);
 			}
 			if(x>4999 && x<6000 || z>4999 && z<6000){
-				e.setDamage(e.getDamage() + 5);
+				e.setDamage(dmg + 5);
 			}
 			if(x>5999 || z>5999){
-				e.setDamage(e.getDamage() + 6);
+				e.setDamage(dmg + 6);
 			}
 			else{}
 		}
